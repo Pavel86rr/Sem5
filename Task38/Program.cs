88d28cbe05fc92a0174e40964d36bf892e3d744a
«Pavel <kskles@yandex.ru>
2023-05-25 18:18:52 +0700

    Задача38

---
+++ Task38/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 38. Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементом 
+// массива
+
+//Random rand = new Random();
+//Console.WriteLine(Math.Round(rand.Next(0,100) + rand.NextDouble(),4));
+
+ double [] FillArray(int size, int leftRange, int rightRange)
+ {
+     double[] tempArray = new  double[size];
+     Random rand = new Random();
+     for(int i=0; i < size; i++)
+     {
+        tempArray [i] = Math.Round((rand.Next(leftRange, rightRange +1) + rand.NextDouble()),3);
+     }
+     return tempArray;
+ }
+  double[] array = FillArray(4, 1,10);
+
+  double max = array[0];
+  double min = array[0];
+  for(int i = 0; i < array.Length; i ++)
+    { 
+        if(max < array[i]) max = array[i];
+        if(min > array[i]) min = array[i]; 
+    }
+ 
+
+void RrintArray(double[]arr)
+{
+   Console.WriteLine("["+ string.Join("  ",arr) + "]"); 
+}
+
+
+RrintArray(array);
+
+Console.WriteLine($"Максимальное значение элемента массива равно: {max}");
+Console.WriteLine($"Минимальное значение элемента массива равно: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива равна: {Math.Round((max-min),3)}");
+
+
+
+
+//  double[] GetRandomArray(int size, int minValue, int maxValue)
+// {
+//     double[] result = new double[size];
+//     var rand = new Random();
+//     for(int i = 0; i < size; i++)
+//     {
+//         result[i] = rand.Next(minValue, maxValue)+rand.NextDouble();
+//     }
+
+//     return result;
+// }
+//  double[] array = GetRandomArray(4, 0, 100);
+// Console.WriteLine($"[{String.Join(", ", array)}]");
